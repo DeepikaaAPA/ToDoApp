fac71cffe3468ead10238b5d018bcab7c75b3c8a
DeepikaaAPA <deepikaarjun@gmail.com>
2024-06-29 07:14:54 +0530

    rem: task header file add to tasks component itself

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -4,9 +4,22 @@ import { TaskList } from "./TaskList";
 export function Tasks({ tasksList, setTasksList }) {
   return (
     <>
-      <TaskHeader
-       
-      ></TaskHeader>
+     <div id="tasks-header">
+        <div>
+          <h3>My ToDos</h3>
+        </div>
+        <div id="filter">
+          <h5>
+            {" "}
+            <label>Status </label>
+            <select id="status-filter">
+              <option value="All">All</option>
+              <option value="Pending">Pending</option>
+              <option value="Completed">Completed</option>
+            </select>
+          </h5>
+        </div>
+      </div>
       <TaskList tasksList={tasksList} setTasksList={setTasksList}></TaskList>
     </>
   );
